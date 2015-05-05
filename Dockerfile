ee33e9aace81766d8f0d974f385690d0713af344
PlsDontPanic <pbelmann@cebitec.uni-bielefeld.de>
2015-05-05 18:52:48 +0200

    schema & cache parameter added

--- Dockerfile
+++ Dockerfile
@@ -4,32 +4,42 @@ MAINTAINER Peter Belmann pbelmann@cebitec.uni-bielefeld.de
 ENV INSTALL_DIR /usr/local/bin/
 
 RUN apt-get update
-RUN apt-get install -y jq wget make g++
+RUN apt-get install -y xz-utils ca-certificates jq wget make g++
 
 # Locations for biobox validator
 ENV BASE_URL  https://s3-us-west-1.amazonaws.com/bioboxes-tools/validate-biobox-file
 ENV VERSION   0.x.y
 ENV VALIDATOR /bbx/validator/
-RUN sudo mkdir -p  ${VALIDATOR} && sudo chmod -R a+wx  /bbx
+RUN mkdir -p  ${VALIDATOR}
 ADD schema.yaml ${VALIDATOR}
 
 # install yaml2json and jq tools
 ENV CONVERT https://github.com/bronze1man/yaml2json/raw/master/builds/linux_386/yaml2json
 RUN cd /usr/local/bin &&  wget --quiet ${CONVERT} && sudo chmod a+x /usr/local/bin/yaml2json
 
+#install jelly-fish
 RUN wget --output-document - http://www.cbcb.umd.edu/software/jellyfish/jellyfish-1.1.11.tar.gz \
            | tar xzf - --directory $INSTALL_DIR  --strip-components=1
-
 RUN cd $INSTALL_DIR && ./configure --prefix=/usr/local/bin && make && make  install 
 ENV PATH ${INSTALL_DIR}bin:$PATH
 
 RUN wget --output-document - https://github.com/DerrickWood/kraken/archive/v0.10.5-beta.tar.gz \
            | tar xzf - --directory $INSTALL_DIR  --strip-components=1
 
+# Install the biobox file validator
+RUN sudo wget \
+      --quiet \
+      --output-document -\
+      ${BASE_URL}/${VERSION}/validate-biobox-file.tar.xz \
+    | sudo tar xJf - \
+      --directory ${VALIDATOR} \
+      --strip-components=1
+ENV PATH ${PATH}:${VALIDATOR}
+
 RUN mkdir -p /bbx/output
 ADD install_kraken.sh $INSTALL_DIR
 ADD run.sh /usr/local/bin/
-ADD tasks /
+ADD Taskfile /
 RUN cd /usr/local/bin && sudo /bin/bash install_kraken.sh /usr/local/bin
 
 ENTRYPOINT ["/usr/local/bin/run.sh"]
--- run.sh
+++ run.sh
@@ -3,29 +3,52 @@
 TASK=$1
 OUTPUT=/bbx/output/bbx
 INPUT=/bbx/input/biobox.yaml
-DB=/tmp
 THREADS=8
 MAX_DB_SIZE=15
-KRAKEN_OUT=/tmp/kraken_out.tsv
 
 #validate yaml
-${VALIDATOR}/validate-biobox-file --schema=${VALIDATOR}schema.yaml --input=$INPUT
+${VALIDATOR}validate-biobox-file --schema=${VALIDATOR}schema.yaml --input=$INPUT
 
 # Run the given task
-CMD=$(egrep ^${TASK}: /tasks | cut -f 2 -d ':')
+CMD=$(egrep ^${TASK}: /Taskfile | cut -f 2 -d ':')
 if [[ -z ${CMD} ]]; then
   echo "Abort, no task found for '${TASK}'."
   exit 1
 fi
 
-#get fasta
-CONTIGS=$(sudo /usr/local/bin/yaml2json < ${INPUT} \
-         | jq --raw-output '.arguments[] | select(has("fasta")) | .fasta.value ')
+TMP_DIR=$(mktemp -d)
+DB=$TMP_DIR
 
-#fetch db
-cd /tmp
-wget http://ccb.jhu.edu/software/kraken/dl/minikraken.tgz 
-tar xf minikraken.tgz --directory $DB --strip-components=1
+KRAKEN_OUT="${TMP_DIR}/kraken_out.tsv"
+
+INPUT_JSON="${TMP_DIR}/biobox.json"
+
+$(yaml2json < ${INPUT} > $INPUT_JSON)
+
+ARGUMENTS=$(jq  --raw-output '.arguments[]' $INPUT_JSON )
+
+#get fasta
+CONTIGS=$( echo $ARGUMENTS | jq --raw-output 'select(has("fasta")) | .fasta.value ' | tr '\n' ' ')
+
+#get cache
+CACHE=$( echo $ARGUMENTS | jq --raw-output 'select(has("cache")) | .cache ' | tr '\n' ' ')
+
+KRAKEN_MINI_URL="http://ccb.jhu.edu/software/kraken/dl/minikraken.tgz"
+
+#check if cache is defined
+if [  -z "$CACHE" ]; then
+
+        # no cache defined -> download the data to temp dir
+        wget --quiet --output-document - $KRAKEN_MINI_URL \
+        |  tar zxvf   - --directory $DB --strip-components=1
+else 
+        DB=$CACHE
+        # cache contains database
+        if [ ! "$(ls -A $DB)" ]; then
+                wget  --quiet --output-document - $KRAKEN_MINI_URL \
+                |  tar zxvf  - --directory $DB --strip-components=1
+        fi
+fi 
 
 eval ${CMD}
 
